e9a2c045b6c815e8c21c2db20ff6e58d6c32a65c
R Holley <rholley@u.rochester.edu>
2020-12-04 11:27:40 -0500

    edits to shiny app

--- JHUCapstone/server.R
+++ JHUCapstone/server.R
@@ -1,76 +1,53 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+## R. Holley
+## text-prediction shiny app, server file
+## Data Science Certificate, JHU/Coursera
 
 library(shiny)
 library(quanteda)
-library(data.table)
 
-# collTable <- readRDS()
-# uniq <- readRDS()
+wordList <- readRDS("./appdict")
 
 ## functions for fetching predictions
 predFun <- function(words, lg){
   words <- words %>% tokens(what="word", remove_punct=TRUE, remove_symbols=TRUE, remove_numbers=TRUE, remove_url=TRUE, remove_separators=TRUE, padding=TRUE) %>% tolower()
-  if(is.known(words)==FALSE){return("Error: No matching predictors")}
+  if(length(words)==0){return("Please enter text on the left.")}
+  if(is.known(words)==FALSE){
+    pred <- wordList$the
+    return(pred[1:lg])
+    }
   if(length(words)==1){
-    pred<-oneword(words, lg)
+    pred <- oneword(words, lg)
     return(pred)
-  } else if(is.known(words)==1){
-    pred<-oneword(words, lg)
-  } else if(length(words)>1){
-    pred<-multiword(words, length(words), lg)
+  }else{
+    i <- length(words)
+    if(is.known(words[i])==FALSE){
+      while(is.known(words[i])==FALSE){
+        i <- i-1
+      }
+    }
+    pred <- oneword(words[i], lg)
     return(pred)
   }
 }
 
 is.known <- function(words){
-  known <- sum(words %in% uniq)
+  known <- sum(words %in% names(wordList))
   if(known==0){return(FALSE)
   }else{(return(known))}
 }
 
-multiword <- function(words, ntok, lg){
-  w2ind<-ntok
-  for(i in 1:ntok){
-    if(is.known(words[w2ind])==1){
-      break()
-    }else{w2ind <- ntok-i}
-  }
-  w1ind<-w2ind-1
-  for(j in 1:w2ind){
-    if(is.known(words[w1ind])==1){
-      break()
-    }else{w1ind<-w2ind-j}
-  }
-  predInd<-which(fullTable$n1==words[w1ind]&fullTable$n2==words[w2ind]&!is.na(fullTable$n3))
-  if(length(predInd)==0){
-    predInd <- which(fullTable$n1==words[w2ind]|fullTable$n1==words[w1ind]&is.na(fullTable$n3))
-    predCol <- "n2"
-  } else {predCol <- "n3"}
-  if(length(predInd)<lg){lg<-length(predInd)}
-  pred <- setorder(fullTable[predInd,c(predCol, "count")], -count)
-  return(pred[1:lg,])
-}
 
 oneword <- function(words, lg){
-  predInd <- which(fullTable$n1==words)
-  pred <- fullTable[predInd, c("n2", "count", "z")]
-  if(length(predInd)<lg){lg<-length(predInd)}
-  return(setorder(pred, -count)[1:lg,])
+  pred <- wordList[[words]]
+  if(lg > length(wordList[[words]])){lg<-length(wordList[[words]])}
+  return(round(pred[1:lg], 3))
 }
 
 ## Server logic starts here
 shinyServer(function(input, output) {
-    output$predict <- renderText({
-      if(length(input$words)==0){return("")}
+    output$predict <- renderTable({
       tok <- as.character(input$words) %>% corpus() %>% corpus_reshape(to="sentences") %>% tolower() %>% tokens(what="word", remove_punct=TRUE, remove_symbols=TRUE, remove_numbers=TRUE, remove_url=TRUE, remove_separators=TRUE, padding=TRUE)
       pred <- predFun(tok[length(tok)], input$length)
-      return(unlist(pred[,1]))
+      return(cbind("Word"=names(pred),"Probability"=pred))
     })
 })
--- JHUCapstone/ui.R
+++ JHUCapstone/ui.R
@@ -1,28 +1,23 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+## R. Holley
+## text-prediction shiny app, ui file
+## Data Science Certificate, JHU/Coursera
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Computer-Speak"),
+    titlePanel("Text Prediction"),
     
     # Intro panel
     wellPanel(
-      p("Welcome to R. Holley's Text Prediction Project! The program was created as the capstone project for Johns Hopkins University/Coursera's Data Science Certificate. *SwiftKey* provided the initial text data. The most recent algorithm update was September 23, 2020.", align="center")),
+      p("Welcome to R. Holley's Text Prediction Project! The most recent update was November 24, 2020.", align="center")),
 
-    # Sidebar with a slider input for number of bins
+    # Sidebar
     sidebarLayout(
         sidebarPanel(
-          sliderInput(inputId="length", label="How many words possible words to return?", min=1, max=15, value = 1),
+          sliderInput(inputId="length", label="How many possible words to return?", min=1, max=15, value = 1),
           "Start typing in the box below.",
             textAreaInput(inputId = "words", label="Input Box",
                           placeholder = "As the great poet Bo Burnham said, 'Words, words, words.'",
@@ -32,14 +27,15 @@ shinyUI(fluidPage(
         # Show a panel of the text + prediction
         mainPanel(
             h3("And the prediction is..."),
-            textOutput(outputId = "predict")
+            tableOutput(outputId = "predict")
         )
     ),
+    # Footer with extra details
     fluidRow(
       column(12,
         h4("Information", align="center"),
-        p("Details about project blah blah blah", align="center"),
-        p("Github info go here", align="center")
+        p("This program was created as the capstone project for Johns Hopkins University/Coursera's Data Science Certificate. SwiftKey provided the text data.", align="center"),
+        p("The model for prediction is a simple Markov-Chain: given the current state, WordA, what are the probable next states? The 'next states' are WordB, WordC, WordX,etc, with probabilities given as the sum of WordA and WordX ordered co-occurances over the total number of WordA occurances. For a more detailed report on the building, cross-validation, and testing of this model, ", tags$a(href="https://github.com/MementoMakoMori/DSCapstone","click here"), "or go to ",tags$a(href="https://github.com/MementoMakoMori/DSCapstone","github.com/MementoMakoMori/DSCapstone"), align="center")
       )
     )
     )
